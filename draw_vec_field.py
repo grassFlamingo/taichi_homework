7007a7b1100027c8bc443d8fd36166af52a39637
grassflamingo <24762386+grassFlamingo@users.noreply.github.com>
2022-09-23 15:30:07 +0800

    optimize draw_vec_field color compute

--- draw_vec_field.py
+++ draw_vec_field.py
@@ -19,6 +19,7 @@ trajectories_status = ti.field(ti.i32, N_PARTICLES)
 box_kb = ti.Vector.field(2, ti.f32, 2)
 
 # time_step = 0.01
+gui_line_color = ti.field(ti.i32, (N_PARTICLES))
 
 @ti.func
 def get_velocity(vec):
@@ -48,11 +49,26 @@ def get_velocity(vec):
     # Vx = x*y - 1
     # Vy = x - y*y*y
 
-    Vx = y + y*y
-    Vy = -0.5 * x + 0.2 * y - x * y + 6.0/5.0 * y*y
+    # Vx = y + y*y
+    # Vy = -0.5 * x + 0.2 * y - x * y + 6.0/5.0 * y*y
+
+    # divergence vector field = 0;
+    # \nabla \cdot B = 0
+    Vx = y
+    Vy = -x
+
 
     return ti.Vector([Vx, Vy])
 
+@ti.func
+def status_to_color(status) -> ti.i32:
+    alpha = max(float(status) / N_TRAJECTORIES, 0.5)
+
+    fcolor = 0xf * (1 - alpha) + 0x2 * alpha
+    color = int(fcolor) & 0xf
+    color = (color << 4) + color # 0xff
+    color = (color << 16) + (color << 8) + color
+    return color
 
 @ti.kernel
 def compute_field():
@@ -62,6 +78,7 @@ def compute_field():
     time_step = 0.05 / box_kb[0][0]
     for i in range(N_PARTICLES):
         status = trajectories_status[i]
+        color = 0
         if status < 0:
             # kept
             pass
@@ -70,16 +87,19 @@ def compute_field():
             if ti.random() < 0.05:
                 trajectories[i, 0] = ti.Vector([ti.random(), ti.random()])
                 status = 1
+                color = status_to_color(status)
         elif status < N_TRAJECTORIES:
             # compute next
             vec = time_step * get_velocity(trajectories[i, status-1])
             trajectories[i, status] = trajectories[i, status-1] + vec
             status += 1
+            color = status_to_color(status)
         else:
             # particle die
             status = 0        
             
         trajectories_status[i] = status
+        gui_line_color[i] = color
 
 gui = ti.GUI("Draw vector field", res=666, background_color=0x222222)
 
@@ -90,6 +110,8 @@ trajectories_status.fill(0)
 
 box_kb[0] = ti.Vector([10, -5])
 box_kb[1] = ti.Vector([10, -5])
+
+gui_line_radius = np.asarray([min(3.0, 0.2*j) for j in range(N_TRAJECTORIES)])
 
 while gui.running:
 
@@ -111,18 +133,10 @@ while gui.running:
     tnp = trajectories.to_numpy()
 
     for i in range(N_PARTICLES):
-        # print("get traj", i, trajectories_status[i])
-        status = float(trajectories_status[i])
-        alpha = max(status / N_TRAJECTORIES, 0.5)
+        color = gui_line_color[i]
+        traj_i = tnp[i]
 
-        color = 0xf * (1 - alpha) + 0x2 * alpha
-        color = int(color) & 0xf
-        color = (color << 4) + color # 0xff
-        color = (color << 16) + (color << 8) + color
-
-        
         for j in range(trajectories_status[i] - 1):
-            gui.line(tnp[i,j], tnp[i,j+1], radius=min(3, 0.2*j), color=color)
-
+            gui.line(traj_i[j], traj_i[j+1], radius=gui_line_radius[j], color=color)
 
     gui.show()
